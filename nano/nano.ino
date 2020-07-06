12a7e49dfc4860cf15b4c259996d2ffa7a446578
berkay3229 <berkayyaman32@gmail.com>
2020-07-06 21:10:14 +0300

    güncelleme

--- nano/nano.ino
+++ nano/nano.ino
@@ -1,12 +1,34 @@
-redpin = 11;
-bluepin = 12;
-greenpin = 13;
+int r_pin = 9;
+int g_pin = 5;
+int b_pin = 11;
+int w_pin = 6;
+
+int r_parlak =255;
+int g_parlak =255;
+int b_parlak =255;
+int w_parlak =255;
+
+byte anahtar = 2;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(r_pin,OUTPUT);
+  pinMode(g_pin,OUTPUT);
+  pinMode(b_pin,OUTPUT);
+  pinMode(w_pin,OUTPUT);
+
+  pinMode(anahtar,INPUT);
+
+  analogWrite(r_pin,r_parlak);
+  analogWrite(g_pin,g_parlak);
+  analogWrite(b_pin,b_parlak);
+  analogWrite(w_pin,w_parlak);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while(anahtar=1){
+    //serial haberleşmeden veri al 
+    }
 
 }
